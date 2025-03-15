cb4e5ad581b559d2126d907f80ab6deebe2ce900
Daniel <danielfernandes202@gmail.com>
2025-03-15 02:12:25 -0300

    fix dir deployment

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,8 @@
-# Exemplo: /home/daniel/apps/meu-projeto/deployment.sh
 #!/bin/bash
+
+# Mudar para o diretório onde o script está localizado
+cd "$(dirname "$0")"
+
 echo "Iniciando deployment..."
 git pull origin master
 docker compose down
