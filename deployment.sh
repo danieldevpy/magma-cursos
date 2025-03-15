75b1bf9a4c1da0a8168dde0717735c9810639bc2
Daniel <danielfernandes202@gmail.com>
2025-03-15 01:36:35 -0300

    fix origin deployment

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 # Exemplo: /home/daniel/apps/meu-projeto/deployment.sh
 #!/bin/bash
 echo "Iniciando deployment..."
-git pull origin main
+git pull origin master
 docker-compose down
 docker-compose up -d --build
 echo "Deployment concluído!"
