a3a60d9ff187db36b3e2f25dbb3b806184bbc1e6
Daniel <danielfernandes202@gmail.com>
2025-03-15 01:41:54 -0300

    fix command git pull in deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 # Exemplo: /home/daniel/apps/meu-projeto/deployment.sh
 #!/bin/bash
 echo "Iniciando deployment..."
-git pull origin master
+git pull -f
 docker-compose down
 docker-compose up -d --build
 echo "Deployment concluído!"
