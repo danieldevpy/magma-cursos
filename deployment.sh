d9fd816e89f6ef4523da4849964f0043f6f03649
Daniel <danielfernandes202@gmail.com>
2025-03-18 17:55:09 -0300

    new file deployment

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,12 @@
 echo "Iniciando deployment..."
+
 echo "Executando git pull..."
+
 git pull origin master
 if [ $? -ne 0 ]; then
     echo "Erro ao executar git pull"
     exit 1
 fi
-
 
 echo "Executando docker compose build"
 docker compose build
@@ -13,16 +14,19 @@ if [ $? -ne 0 ]; then
     echo "Erro ao executar docker compose build"
     exit 1
 fi
-echo "Executando docker compose down..."
-docker compose down
+
+echo "Executando docker compose stop..."
+docker compose stop django-magma
 if [ $? -ne 0 ]; then
-    echo "Erro ao executar docker compose down"
+    echo "Erro ao executar docker compose stop"
     exit 1
 fi
+
 echo "Executando docker compose up..."
-docker compose up -d
+docker compose up django-magma -d
 if [ $? -ne 0 ]; then
     echo "Erro ao executar docker compose up"
     exit 1
 fi
+
 echo "Deployment concluído!"
